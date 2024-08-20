bf63d3bdc2fbd2fff87c58d41af7ac1004910789
HANG Tao (BCSC-EPA1, XC-DX/PJ-W3-PMT) <Tao.HANG@cn.bosch.com>
2024-08-20 10:15:57 +0800

    add log and exception hanlding

--- dynamic_rostopic_subscriber/subscriber.py
+++ dynamic_rostopic_subscriber/subscriber.py
@@ -2,21 +2,28 @@
 Author       : HANG Tao (BCSC-EPA1, XC-DX/PJ-W3-PMT) Tao.HANG@cn.bosch.com
 Date         : 2024-08-19 23:23:52
 LastEditors  : HANG Tao (BCSC-EPA1, XC-DX/PJ-W3-PMT) Tao.HANG@cn.bosch.com
-LastEditTime : 2024-08-19 23:50:15
+LastEditTime : 2024-08-20 10:14:58
 FilePath     : /DynamicROSTopicSubscriber/dynamic_rostopic_subscriber/subscriber.py
 Description  : 
 
 Copyright (c) 2024 by Robert Bosch GmbH, All Rights Reserved. 
 """
 
+import logging
 from functools import wraps
 
 import rospy
 from genpy.dynamic import generate_dynamic
 from rospy import AnyMsg
 
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
-# 单例装饰器，确保类只有一个实例
+
+# Singleton decorator to ensure only one instance of the class
 def singleton(cls):
     instances = {}
 
@@ -29,27 +36,27 @@ def singleton(cls):
 
 
 @singleton
-class DynamicTopicSubscriber:
+class DynamicROSTopicSubscriber:
     """
-    动态 ROS 主题订阅器类
-    允许动态订阅 ROS 主题并处理消息
+    Dynamic ROS topic subscriber class
+    Allows dynamic subscription to ROS topics and handles messages
     """
 
-    def __init__(self, node_name="dynamic_topic_subscriber"):
+    def __init__(self, node_name="dynamic_ros_topic_subscriber"):
         """
-        初始化订阅器
-        :param node_name: ROS 节点名称
+        Initialize the subscriber
+        :param node_name: ROS node name
         """
-        self.subscribers = {}  # 存储主题和回调函数的字典
-        self.cached_messages = {}  # 缓存已生成的消息类型
-        self.node_initialized = False  # 标记 ROS 节点是否已初始化
-        self.node_name = node_name  # ROS 节点名称
+        self.subscribers = {}  # Dictionary to store topics and callback functions
+        self.cached_messages = {}  # Cache for generated message types
+        self.node_initialized = False  # Flag to check if ROS node is initialized
+        self.node_name = node_name  # ROS node name
 
     def subscribe(self, topic, msg_type=AnyMsg):
         """
-        订阅主题的装饰器
-        :param topic: 要订阅的 ROS 主题
-        :param msg_type: 消息类型，默认为 AnyMsg
+        Decorator for subscribing to a topic
+        :param topic: ROS topic to subscribe to
+        :param msg_type: Message type, default is AnyMsg
         """
 
         def decorator(func):
@@ -57,16 +64,25 @@ class DynamicTopicSubscriber:
             def wrapper(*args, **kwargs):
                 return func(*args, **kwargs)
 
-            if not self.node_initialized:
-                rospy.init_node(self.node_name, anonymous=True)
-                self.node_initialized = True
+            try:
+                if not self.node_initialized:
+                    rospy.init_node(self.node_name, anonymous=True)
+                    self.node_initialized = True
 
-            if topic not in self.subscribers:
-                self.subscribers[topic] = wrapper
-                rospy.Subscriber(topic, msg_type, self.callback)
-                print(f"Subscribed to topic: {topic}")
-            else:
-                print(f"Already subscribed to topic: {topic}")
+                if topic not in self.subscribers:
+                    self.subscribers[topic] = wrapper
+                    rospy.Subscriber(topic, msg_type, self.callback)
+                    logger.info(f"Subscribed to topic: {topic}")
+                else:
+                    logger.info(f"Already subscribed to topic: {topic}")
+            except rospy.ROSException as e:
+                logger.error(
+                    f"ROS Exception occurred while subscribing to {topic}: {e}"
+                )
+            except Exception as e:
+                logger.exception(
+                    f"Unexpected error occurred while subscribing to {topic}: {e}"
+                )
 
             return wrapper
 
@@ -74,35 +90,47 @@ class DynamicTopicSubscriber:
 
     def spin(self):
         """
-        开始处理 ROS 消息
+        Start processing ROS messages
         """
-        rospy.spin()
+        try:
+            rospy.spin()
+        except rospy.ROSInterruptException:
+            logger.info("ROS node shut down")
+        except Exception as e:
+            logger.exception(f"Unexpected error occurred during spin: {e}")
 
     def callback(self, msg: AnyMsg):
         """
-        处理接收到的 ROS 消息
-        :param msg: 接收到的 ROS 消息
+        Handle received ROS messages
+        :param msg: Received ROS message
         """
-        connection_header = msg._connection_header
+        try:
+            connection_header = msg._connection_header
 
-        topic = connection_header.get("topic", "")
-        md5sum = connection_header.get("md5sum", "")
-        if md5sum in self.cached_messages:
-            msg_instance = self.cached_messages[md5sum]
-        else:
-            # 生成动态消息
-            msg_type = connection_header.get("type", "")
-            message_definition = connection_header.get("message_definition", "")
-            msg_spec = generate_dynamic(msg_type, message_definition)
+            topic = connection_header.get("topic", "")
+            md5sum = connection_header.get("md5sum", "")
+            if md5sum in self.cached_messages:
+                msg_instance = self.cached_messages[md5sum]
+            else:
+                # Generate dynamic message
+                msg_type = connection_header.get("type", "")
+                message_definition = connection_header.get("message_definition", "")
+                msg_spec = generate_dynamic(msg_type, message_definition)
 
-            for _, msg_cls in msg_spec.items():
-                self.cached_messages[msg_cls._md5sum] = msg_cls()
+                for _, msg_cls in msg_spec.items():
+                    self.cached_messages[msg_cls._md5sum] = msg_cls()
 
-            msg_instance = self.cached_messages[md5sum]
+                msg_instance = self.cached_messages[md5sum]
 
-        deserialized_msg = msg_instance.deserialize(msg._buff)
-        self.subscribers[topic](deserialized_msg)
+            deserialized_msg = msg_instance.deserialize(msg._buff)
+            self.subscribers[topic](deserialized_msg)
+        except KeyError as e:
+            logger.error(f"KeyError occurred while processing message: {e}")
+        except AttributeError as e:
+            logger.error(f"AttributeError occurred while processing message: {e}")
+        except Exception as e:
+            logger.exception(f"Unexpected error occurred while processing message: {e}")
 
 
-# 创建全局实例
-ros_subscriber = DynamicTopicSubscriber()
+# Create global instance
+ros_subscriber = DynamicROSTopicSubscriber()
